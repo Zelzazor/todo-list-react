bf300f63870384c9d73e7d5db4c6f0e8e4e9ec54
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-11-25 21:03:03 -0400

    agregadas opciones de edicion y eliminación de projectos

--- src/components/MainSide.jsx
+++ src/components/MainSide.jsx
@@ -1,5 +1,8 @@
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import {faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 
 function MainSide(props) {
   const {project, editProject} = props;
@@ -10,6 +13,14 @@ function MainSide(props) {
           <div className="project-options">
             <div className="project-info">
               <h1>{project.title}</h1>
+            </div>
+            <div className="project-buttons">
+              <button id="btnEdit">
+                <FontAwesomeIcon icon={faEdit} />
+              </button>
+              <button id="btnDelete">
+                <FontAwesomeIcon icon={faTrashAlt} />
+              </button>
             </div>
           </div>
         </header>
